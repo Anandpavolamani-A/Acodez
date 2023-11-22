9d41fc706efc8b149a9980d97ccdb55523adcc27
Anandpavolamani-A <149556812+Anandpavolamani-A@users.noreply.github.com>
2023-11-22 14:43:06 +0530

    Add files via upload

---
+++ ACODEZ/structure/javascript/index.js
@@ -0,0 +1,44 @@
+function questions1() {
+    var acc = document.getElementsByClassName("content1");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}
+function questions2() {
+    var acc = document.getElementsByClassName("content2");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}function questions3() {
+    var acc = document.getElementsByClassName("content3");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}function questions4() {
+    var acc = document.getElementsByClassName("content4");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}function questions5() {
+    var acc = document.getElementsByClassName("content5");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}function questions6() {
+    var acc = document.getElementsByClassName("content6");
+    if (acc[0].className.includes("newcontent")) {
+        acc[0].classList.remove('newcontent');
+    } else {
+        acc[0].classList.add('newcontent');
+    }
+}
